f3bf4b737cc0d485c4603a0569e795d1796b9cfa
Maroon152002 <danialmaroon15@gmail.com>
2024-08-29 12:05:47 +0530

    commit

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,32 +1,38 @@
 import React from 'react'
 import './LandPage.css';
+import './blocks.scss';
 
 export default function LandingPage() {
     return (
-        <div className="landing-page">
-            <div className="img">
-                <img src="/images/Picture1.jpeg" alt="boy" />
+        <div className="main-landing-page">
+            
+            {/* <div className="men-img">
+                <img src="/images/capman.png" alt="men" />
+            </div> */}
+
+            <div className="hero">
+                <div class="hero__title"></div>
+                <div class="cube"></div>
+                <div class="cube"></div>
+                <div class="cube"></div>
+                <div class="cube"></div>
+                <div class="cube"></div>
+                <div class="cube"></div>
             </div>
 
+
+            
+
             <div className="wave-image">
-                <img src="/images/wavesvg.svg" alt="wave" />
+                <img src="/images/wave1.jpeg" alt="wave" />
             </div>
 
             <div className="innate-logo">
                 <img src="/images/innate.jpeg" alt="innate" />
             </div>
 
-            <div className="boygif">
-                <img src="/images/gif1.gif" alt="gif" />
-            </div>
+            
 
-            <div className="camera">
-                <img src="" alt="" />
-            </div>
-
-            <div className="girlgif">
-                <img src="/images/gif2.gif" alt="gif" />
-            </div>
 
         </div>
     );
